5b2b260afe551f791573170d55c567c90e04d16b
PDarrall <paul@gamevy.com>
2017-04-18 14:49:10 +0100

    Update bundle.js

--- public/bundle.js
+++ public/bundle.js
@@ -8147,7 +8147,7 @@
 		$('#lobby-privacy').toggle(privateGame);
 		if (privateGame) {
 			var gid = data.gid;
-			$('#lobby-private-code').html('<a href="/join/'+gid+'" target="_blank">https://secrethitler.games/join/<strong>' + gid + '</strong></a>');
+			$('#lobby-private-code').html('<a href="/join/'+gid+'" target="_blank">http://secrethitlergames.herokuapp.com/join/<strong>' + gid + '</strong></a>');
 		}
 	};
 
