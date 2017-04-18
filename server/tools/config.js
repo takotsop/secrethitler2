acd96f61b53073ac9341e3b86029e11f11145fac
PDarrall <paul@gamevy.com>
2017-04-18 14:25:27 +0100

    Update config.js
    Add Key

--- server/tools/config.js
+++ server/tools/config.js
@@ -2,7 +2,7 @@
 
 module.exports = {
 
-	LOCAL_DB_URL: process.env.DATABASE_URL || '',
+	LOCAL_DB_URL: process.env.DATABASE_URL || 'postgres://ypjywyuowwkvsx:a3273a00673f833d479e1ab366c5cae9d6de8df35465ef8002e815d969d229b1@ec2-54-228-235-185.eu-west-1.compute.amazonaws.com:5432/d3ilfgnfcibavf',
 
 	SENDGRID_API_KEY: process.env.SENDGRID_API_KEY || '',
 
